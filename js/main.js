2848c49c18c1ee36f442f4d1e2472a73738d9716
Tine-Dahlin <56149807+Tine-Dahlin@users.noreply.github.com>
2020-10-01 14:10:09 +0200

    mange ændringer her

--- js/main.js
+++ js/main.js
@@ -101,3 +101,81 @@ function initComparisons() {
 initComparisons();
 
 
+
+
+//pop op vindue til projekter
+// Få modal elementet
+const modal = document.getElementById('simpleModal');
+const modal2 = document.getElementById('simpleModal2');
+const modal3 = document.getElementById('simpleModal3');
+//Åben modal button
+const modalBtn = document.getElementById('modalBtn');
+const modalBtn2 = document.getElementById('modalBtn2');
+const modalBtn3 = document.getElementById('modalBtn3');
+// få lukke button
+const closeBtn = document.getElementsByClassName('closeBtn')[0];
+const closeBtn2 = document.getElementsByClassName('closeBtn')[1];
+const closeBtn3 = document.getElementsByClassName('closeBtn')[2];
+
+
+//åbne klik her
+modalBtn.addEventListener('click', function () {
+    openModal(modal);
+});
+
+modalBtn2.addEventListener('click', function () {
+    openModal(modal2);
+});
+
+modalBtn3.addEventListener('click', function () {
+    openModal(modal3);
+});
+
+
+
+
+//lukke klik her
+closeBtn.addEventListener('click', function () {
+    closeModal(modal);
+
+});
+
+
+closeBtn2.addEventListener('click', function () {
+    closeModal(modal2);
+
+});
+
+closeBtn3.addEventListener('click', function () {
+    closeModal(modal3);
+
+});
+
+
+
+//lukke klik udenfor boksen
+window.addEventListener('click', outsideclick);
+
+
+
+//function til at åbne modal
+function openModal(modal) {
+    modal.style.display = 'block';
+}
+
+//function til at lukke modal
+function closeModal(modal) {
+    modal.style.display = 'none';
+}
+
+//function til at lukke modal udenfor klik
+function outsideclick(e) {
+    if (e.target == modal) {
+        modal.style.display = 'none';
+    } else if (e.target == modal2) {
+        modal2.style.display = 'none';
+    } else if (e.target == modal3) {
+        modal3.style.display = 'none';
+    }
+}
+
